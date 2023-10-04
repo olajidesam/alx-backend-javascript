94197c22b865b6f5234e7e10623f71e530d53eb9
Dada Olajide <olajidedada74@gmail.com>
2023-10-04 03:28:50 +0100

    E6 classes

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,32 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this.size = size;
+    this.location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set size(population) {
+    this._size = population;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set location(pin) {
+    this._location = pin;
+  }
+
+  [Symbol.toPrimitive](cast) {
+    if (cast === 'number') {
+      return this.size;
+    }
+    if (cast === 'string') {
+      return this.location;
+    }
+    return this;
+  }
+}
